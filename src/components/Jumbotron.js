3f0d54db9d289582b723e92ebf4244474863ec64
DanLowo <danlowo@gmail.com>
2021-10-08 01:39:21 +0100

    jumbotron component

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../assets/css/Jumbotron.css"
+
+const Jumbotron = () => {
+    return (
+        <div id="Jumbotron">
+            {/* <p>dipsi</p> */}
+        </div>
+    )
+}
+
+export default Jumbotron;
