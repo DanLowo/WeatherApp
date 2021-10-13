dbf432bf5524b8e79a3d6089b8890ce6db429553
DanLowo <danlowo@gmail.com>
2021-10-13 19:02:54 +0100

    fetch all weather details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import "./assets/css/App.css"
 import Jumbotron from "./Jumbotron";
 import 'material-design-icons/iconfont/material-icons.css'
-import Main from "./components/Main";
+import Main from "./Main";
 
+// redux
+import { getWeather } from './store/weather'
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getWeather())
+  })
+
   return (
     <div className="App">
       <Jumbotron />
