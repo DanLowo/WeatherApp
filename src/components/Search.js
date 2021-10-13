8b3db0d38a06c463b4b68da3da41029e48691a0c
DanLowo <danlowo@gmail.com>
2021-10-13 11:33:10 +0100

    search component

---
+++ src/components/Search.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Icon from './Icon'
+
+const Search = ({ closeSearch }) => {
+    const [searchText, setSearchText] = useState("")
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        console.log(searchText);
+    }
+
+    const LocationItem = ({location}) => (
+        <div className="location-item">
+            <p className="location-item-text">{location}</p>
+            <span className="location-item-icon">
+                <Icon name="chevron_right" />
+            </span>
+        </div>
+    )
+
+    return (
+        <div id="search">
+            <section align="right" onClick={closeSearch} >
+                <Icon name="face" />
+            </section>
+            <div className="search-form">
+                <form onSubmit={handleSearch}>
+                    <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="search location" />
+                    <button type="submit" id="submit-button">Search</button>
+                </form>
+            </div>
+
+            <div className="locations">
+                <LocationItem location="Lagos" />
+                <LocationItem location="Berlin" />
+                <LocationItem location="Bercelonia" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Search
