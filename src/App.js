f4518b847bdcb8c7d72b32ce3fc158fb1d64844f
DanLowo <danlowo@gmail.com>
2021-10-13 11:35:12 +0100

    add main component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./assets/css/App.css"
 import Jumbotron from "./Jumbotron";
+import 'material-design-icons/iconfont/material-icons.css'
+import Main from "./components/Main";
 
 
 function App() {
   return (
     <div className="App">
       <Jumbotron />
+      <Main />
     </div>
   );
 }
