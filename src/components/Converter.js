9b7420f849d4a4a9bfa06677714bd83227dd7988
DanLowo <danlowo@gmail.com>
2021-10-13 14:25:45 +0100

    c-to-f converter

---
+++ src/components/Converter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from './Icon'
+
+const Converter = () => {
+    return (
+        <div align="right">
+            <Icon name="face" background />
+            <span style={{marginLeft: 10}} />
+            <Icon name="face" background />
+        </div>
+    )
+}
+
+export default Converter
