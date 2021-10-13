76a83f462c1ab805977b2fdb692b01479fb9a17c
DanLowo <danlowo@gmail.com>
2021-10-13 19:00:48 +0100

    use dynamic temperature

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import '../assets/css/WeatherDetails.css'
+import convert from '../util/convert'
 import Icon from './Icon'
 
 const WeatherDetails = ({ degree, type, date, location }) => {
+    const [temp, setTemp] = useState(0)
+    const { unit } = useSelector(state => state.weather)
+
+    useEffect(() => {
+        const degreeNum = () => {
+            const ok = convert(Math.round(degree), unit)
+            setTemp(ok)
+        }
+        degreeNum()
+    }, [unit, degree])
+
     return (
         <div id="weather-details">
             <h1 id="weather-degree">
-                {degree}
-                <span id="weather-degree-symbol">oC</span>
+                {temp}
+                <span id="weather-degree-symbol">o{unit}</span>
             </h1>
             <h3 className="weather-type">{type}</h3>
             <p className="current-date">
