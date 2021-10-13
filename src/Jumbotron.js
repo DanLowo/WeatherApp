2b0151eee470fae32e8a7943eaf2637a04b9e76d
DanLowo <danlowo@gmail.com>
2021-10-13 11:32:45 +0100

    add search and current weather component

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./assets/css/Jumbotron.css"
+import CurrentWeather from './components/CurrentWeather'
+import Search from './components/Search'
 
 const Jumbotron = () => {
+    const [isSearch, setIsSearch] = useState(false)
+
+    const controlSearch = (state) => {
+        setIsSearch(!isSearch)
+    }
+
     return (
         <div id="Jumbotron">
-            
+            {isSearch ? <Search closeSearch={controlSearch} /> : <CurrentWeather openSearch={controlSearch} />}
         </div>
     )
 }
